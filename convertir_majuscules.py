25cca44f5b2d44b55d16d0b9d946db37c13219e9
Othman Bella <147095415+Othman-Id-bella@users.noreply.github.com>
2023-11-16 00:49:34 +0100

    Add files via upload

---
+++ convertir_majuscules.py
@@ -0,0 +1,7 @@
+def convertir_majuscules(chaine):
+    return chaine.upper()
+
+chaine_originale = "Hello, World!"
+chaine_majuscules = convertir_majuscules(chaine_originale)
+print(f"Chaine originale : {chaine_originale}")
+print(f"Chaine en majuscules : {chaine_majuscules}")
